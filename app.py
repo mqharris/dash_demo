445c542a8895d1422bf479e0e049800347c41b52
Mitch Harris <mitchharris@live.ca>
2021-03-03 09:56:58 -0800

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+"""
+testing hosting on heroku
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        "How casual are we talking?",
+        dcc.Slider(min=0, max=5, value=2, marks={0: "0", 5: "5"}),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
